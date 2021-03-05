9346f9fd304bcfdba8a6fc9b84ed36f732169d7b
Giorgi Shelia <52777715+giorgishelia12@users.noreply.github.com>
2021-03-06 02:21:06 +0400

    initial commit
    added everything

---
+++ index.js
@@ -0,0 +1,28 @@
+
+document.getElementById('but').addEventListener("click",
+function kvadrat() {
+    let a = document.getElementById("a").value;
+    let b = document.getElementById("b").value;
+    let c = document.getElementById('c').value;
+    let result = document.getElementById('result');
+
+
+    let D = b * b - 4 * a * c;
+    let x1;
+    let x2;
+
+    if(D > 0){
+        x1  = (-b + Math.sqrt(D)) / (2*a);
+        x2  = (-b - Math.sqrt(D)) / (2*a)
+        console.log(Math.floor(x1),Math.floor(x2),D);
+        result.innerText = Math.floor(x1) + " " + Math.floor(x2) + " " + D; 
+    }else if(D == 0){
+        x1 = x2 = -b /(2*a);
+        console.log(x1, x2);
+        result.innerText = x1 + " " + x2;
+    }else if(D < 0){ 
+        console.log("cudia brat araa");
+        result.innerText = "cudia brat araa";
+    }
+
+});
